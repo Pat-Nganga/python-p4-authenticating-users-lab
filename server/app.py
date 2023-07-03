c0fddca7f13e25a81c43f57f279a95e7aea58967
Pat <wambuipatricia3@gmail.com>
2023-07-03 21:46:37 +0300

    Completed Assignment

--- server/app.py
+++ server/app.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python3
-
 from flask import Flask, make_response, jsonify, request, session
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
@@ -47,10 +45,31 @@ class ShowArticle(Resource):
             return make_response(article_json, 200)
 
         return {'message': 'Maximum pageview limit reached'}, 401
+class Login(Resource):
+    def post(self):
+        user = User.query.filter(User.username==request.get_json()["username"]).first()
+        session["user_id"] = user.id
+        return make_response(jsonify(user.to_dict()), 200)
+    
+class Logout(Resource):
+    def delete(self):
+        session.pop('user_id', None)
+        return '', 204
+
+class CheckSession(Resource):
+    def get(self):
+        user = User.query.filter(User.id==session.get("user_id")).first()
+        if user:
+           return make_response(jsonify(user.to_dict()), 200)
+        return make_response(jsonify({}), 401)
+
 
 api.add_resource(ClearSession, '/clear')
 api.add_resource(IndexArticle, '/articles')
 api.add_resource(ShowArticle, '/articles/<int:id>')
+api.add_resource(Login, '/login')
+api.add_resource(Logout, '/logout')
+api.add_resource(CheckSession, '/check_session')
 
 
 if __name__ == '__main__':
